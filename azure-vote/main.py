e642599e7a48a228dc4c7db2baf90ee88cbbc3c5
Oops! <26shiine@gmail.com>
2024-05-12 01:07:33 +0700

    update redis and run app

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -85,7 +85,20 @@ else:
     title = app.config['TITLE']
 
 # Redis Connection
-r = redis.Redis()
+# The commented section below is used while deploying the application with two separate containers - 
+# One container for Redis and another for the frontend. 
+# Redis configurations
+redis_server = os.environ['REDIS']
+try:
+    if "REDIS_PWD" in os.environ:
+        r = redis.StrictRedis(host=redis_server,
+                        port=6379,
+                        password=os.environ['REDIS_PWD'])
+    else:
+        r = redis.Redis(redis_server)
+    r.ping()
+except redis.ConnectionError:
+    exit('Failed to connect to Redis, terminating.')
 
 # Change title to host name to demo NLB
 if app.config['SHOWHOST'] == "true":
@@ -146,6 +159,6 @@ def index():
 
 if __name__ == "__main__":
     # TODO: Use the statement below when running locally
-    app.run() 
+    # app.run() 
     # TODO: Use the statement below before deployment to VMSS
     # app.run(host='0.0.0.0', threaded=True, debug=True) # remote
